dc971db5f8345f8b6b4c5e602c092262af5b3f0d
Nurimansyah Rifwan <nurimansyah.rifwan@gmail.com>
2016-12-03 13:04:58 +0700

    Finished base structure for “Elevate Framework”. And updating the README file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,8 +17,16 @@
         /* ---------- | Load Grunt Tasks | ---------- */
         require('load-grunt-tasks')(grunt);
 
+        /* ---------- | Variable Init | ---------- */
+        var noserve = grunt.option('noserve') || false;
+
         /* ---------- | Configuration Init | ---------- */
         var configs = {
+            // File Name Output
+            files: {
+                script:     'bundle'
+            },
+
             // Source Directory Structure
             srcs: {
                 root:       './srcs/',
@@ -30,14 +38,14 @@
             },
             // Target Directory Environment
             target: {
-                // Test
+                // Test (This Environment is for Contributor Only)
                 test:           './framework-tests/',
                 testAssets:     'assets/',
                 testStatic:     'static/',
                 testCSS:        'css/',
                 testScripts:    'scripts/',
                 // Development
-                dev:            './dev/',
+                dev:            './dist/',
                 devAssets:      'assets/',
                 devStatic:      'static/',
                 devCSS:         'css/',
@@ -56,11 +64,181 @@
             pkg: grunt.file.readJSON('package.json'),
 
             /* ---------- | Task Lists | ---------- */
-            // Uglify Task
-            uglify: {
+            // BrowserSync Task
+            browserSync: {
+                // Test Environment
+                test: {
+                    options: {
+                        watchTask: true,
+                        server: configs.target.test
+                    },
+                    bsFiles: {
+                        src: [
+                            configs.target.test + '*.html',
+                            configs.target.test + '**/*.html',
+                            configs.target.test + configs.target.testStatic + configs.target.testCSS + '*.css',
+                            configs.target.test + configs.target.testStatic + configs.target.testScripts + '*.js'
+                        ]
+                    }
+                },
+
                 // Development Environment
                 dev: {
-                    files: {}
+                    options: {
+                        watchTask: true,
+                        server: configs.target.dev
+                    },
+                    bsFiles: {
+                        src: [
+                            configs.target.dev + '*.html',
+                            configs.target.dev + '**/*.html',
+                            configs.target.dev + configs.target.devStatic + configs.target.devCSS + '*.css',
+                            configs.target.dev + configs.target.devStatic + configs.target.devScripts + '*.js'
+                        ]
+                    }
+                },
+
+                // Production Environment
+                prod: {
+                    options: {
+                        watchTask: true,
+                        server: configs.target.prod
+                    },
+                    bsFiles: {
+                        src: [
+                            configs.target.prod + '*.html',
+                            configs.target.prod + '**/*.html',
+                            configs.target.prod + configs.target.prodStatic + configs.target.prodCSS + '*.css',
+                            configs.target.prod + configs.target.prodStatic + configs.target.prodScripts + '*.js'
+                        ]
+                    }
+                }
+            },
+
+            // Copy Task
+            copy: {
+                // Test Environment
+                test: {
+                    files: [{
+                        expand: true,
+                        cwd:    configs.srcs.root + configs.srcs.assets,
+                        src:    ['**/*.*', '!**/*.{png,jpg,gif}'],
+                        dest:   configs.target.test + configs.target.testStatic + configs.target.testAssets
+                    }]
+                },
+
+                // Development Environment
+                dev: {
+                    files: [{
+                        expand: true,
+                        cwd:    configs.srcs.root + configs.srcs.assets,
+                        src:    ['**/*.*', '!**/*.{png,jpg,gif}'],
+                        dest:   configs.target.dev + configs.target.devStatic + configs.target.devAssets
+                    }]
+                },
+
+                // Production Environment
+                prod: {
+                    files: [{
+                        expand: true,
+                        cwd:    configs.srcs.root + configs.srcs.assets,
+                        src:    ['**/*.*', '!**/*.{png,jpg,gif}'],
+                        dest:   configs.target.prod + configs.target.prodStatic + configs.target.prodAssets
+                    }]
+                }
+            },
+            
+            // Image Minified Task
+            imagemin: {
+                // Test Environment
+                test: {
+                    files: [{
+                        expand: true,
+                        cwd:    configs.srcs.root + configs.srcs.assets,
+                        src:    ['**/*.{png,jpg,gif}'],
+                        dest:   configs.target.test + configs.target.testStatic + configs.target.testAssets
+                    }]
+                },
+                // Development Environment
+                dev: {
+                    files: [{
+                        expand: true,
+                        cwd:    configs.srcs.root + configs.srcs.assets,
+                        src:    ['**/*.{png,jpg,gif}'],
+                        dest:   configs.target.dev + configs.target.devStatic + configs.target.devAssets
+                    }]
+                },
+                // Production Environment
+                prod: {
+                    files: [{
+                        expand: true,
+                        cwd:    configs.srcs.root + configs.srcs.assets,
+                        src:    ['**/*.{png,jpg,gif}'],
+                        dest:   configs.target.prod + configs.target.prodStatic + configs.target.prodAssets
+                    }]
+                }
+            },
+
+            // JS Hint Task
+            jshint: {
+                options: {
+                    reporter: require('jshint-stylish')
+                },
+                all: [
+                    configs.srcs.root + configs.srcs.scripts + '**/*.js'
+                ]
+            },
+
+            // Bake Task
+            bake: {
+                // Testing Environment
+                test: {
+                    options: {},
+                    files: [{
+                        expand: true,
+                        cwd:    configs.srcs.root + configs.srcs.test,
+                        src:    ['**/*.html', '!**/_*.html'],
+                        dest:   configs.target.test,
+                        ext:    '.html'
+                    }]
+                },
+
+                // Development Environment
+                dev: {
+                    options: {},
+                    files: [{
+                        expand: true,
+                        cwd:    configs.srcs.root + configs.srcs.html,
+                        src:    ['**/*.html', '!**/_*.html'],
+                        dest:   configs.target.dev,
+                        ext:    '.html'
+                    }]
+                },
+
+                // Production Environment
+                prod: {
+                    options: {},
+                    files: [{
+                        expand: true,
+                        cwd:    configs.srcs.root + configs.srcs.html,
+                        src:    ['**/*.html', '!**/_*.html'],
+                        dest:   configs.target.prod,
+                        ext:    '.html'
+                    }]
+                }
+            },
+
+            // Uglify Task
+            uglify: {
+                // Production Environment
+                prod: {
+                    files: [{
+                        expand: true,
+                        cwd:    configs.target.prod + configs.target.prodStatic + configs.target.prodScripts,
+                        src:    configs.files.script + '.js',
+                        dest:   configs.target.prod + configs.target.prodStatic + configs.target.prodScripts,
+                        ext:    '.min.js'
+                    }]
                 }
             },
 
@@ -70,8 +248,24 @@
                 test: {
                     entry: [configs.srcs.root + configs.srcs.scripts + 'script.js'],
                     output: {
-                        filename:   'bundle.js',
-                        path:       configs.target.test + configs.target.testScripts
+                        filename:   configs.files.script + '.js',
+                        path:       configs.target.test + configs.target.testStatic + configs.target.testScripts
+                    }
+                },
+                // Development Environment
+                dev: {
+                    entry: [configs.srcs.root + configs.srcs.scripts + 'script.js'],
+                    output: {
+                        filename:   configs.files.script + '.js',
+                        path:       configs.target.dev + configs.target.devStatic + configs.target.devScripts
+                    }
+                },
+                // Peoduction Environment
+                prod: {
+                    entry: [configs.srcs.root + configs.srcs.scripts + 'script.js'],
+                    output: {
+                        filename:   configs.files.script + '.js',
+                        path:       configs.target.prod + configs.target.prodStatic + configs.target.prodScripts
                     }
                 }
             },
@@ -87,7 +281,7 @@
                         expand: true,
                         cwd:    configs.srcs.root + configs.srcs.scss,
                         src:    ['*.scss'],
-                        dest:   configs.target.test + configs.target.testCSS,
+                        dest:   configs.target.test + configs.target.testStatic + configs.target.testCSS,
                         ext:    '.css'
                     }]
                 },
@@ -100,7 +294,7 @@
                         expand: true,
                         cwd:    configs.srcs.root + configs.srcs.scss,
                         src:    ['*.scss'],
-                        dest:   configs.target.dev + configs.target.devCSS,
+                        dest:   configs.target.dev + configs.target.devStatic + configs.target.devCSS,
                         ext:    '.css'
                     }]
                 },
@@ -111,7 +305,7 @@
                         expand: true,
                         cwd:    configs.srcs.root + configs.srcs.scss,
                         src:    ['*.scss'],
-                        dest:   configs.target.prod + configs.target.prodCSS,
+                        dest:   configs.target.prod + configs.target.prodStatic + configs.target.prodCSS,
                         ext:    '.min.css'
                     }]
                 }
@@ -120,29 +314,89 @@
             // Clean Task
             clean: {
                 // Test
-                test: [
-                    configs.target.test + configs.target.testCSS,
-                    configs.target.test + configs.target.testScripts
-                ],
+                test: [configs.target.test],
                 // Development
-                dev: [
-                    configs.target.dev + configs.target.devCSS
-                ],
-                // Clean All
-                all: [
-                    // Test Env.
-                    configs.target.test,
-                    // Development Env.
-                    configs.target.dev
+                dev: [configs.target.dev],
+                // Production
+                prod: [configs.target.prod],
+                // Uglify
+                uglify: [
+                    configs.target.prod +
+                    configs.target.prodStatic +
+                    configs.target.prodScripts +
+                    configs.files.script + '.js'
                 ]
             },
 
             // Concurrent Task
             concurrent: {
+                // Test Environment
                 test: [
                     ['clean:test'],
-                    ['sass:test', 'webpack:test'],
+                    'imagemin:test',
+                    'copy:test',
+                    'webpack:test',
+                    'bake:test',
+                    'sass:test',
+                    'jshint',
                     'notify'
+                ],
+                testWatch: [
+                    'watch:testHTML',
+                    'watch:testAssets',
+                    'watch:testSASS',
+                    'watch:testScript',
+                    'jshint'
+                ],
+
+                // Development Environment
+                dev: [
+                    ['clean:dev'],
+                    [
+                        'imagemin:dev',
+                        'copy:dev',
+                        'webpack:dev',
+                        'bake:dev',
+                        'sass:dev'
+                    ],
+                    'jshint',
+                    'notify'
+                ],
+                devWatch: [
+                    [
+                        'watch:devHTML',
+                        'watch:devAssets',
+                        'watch:devSASS',
+                        'watch:devScript'
+                    ],
+                    'jshint'
+                ],
+
+                // Production Environment
+                prod: [
+                    ['clean:prod'],
+                    [
+                        'imagemin:prod',
+                        'copy:prod',
+                        'webpack:prod',
+                        'bake:prod',
+                        'sass:prod',
+                        'uglify',
+                        'clean:uglify'
+                    ],
+                    'jshint',
+                    'notify'
+                ],
+                prodWatch: [
+                    [
+                        'watch:prodHTML',
+                        'watch:prodAssets',
+                        'watch:prodSASS',
+                        'watch:prodScript'
+                    ],
+                    'jshint',
+                    'uglify',
+                    'clean:uglify'
                 ]
             },
 
@@ -150,21 +404,99 @@
             notify: {
                 clean: {
                     options: {
-                        title: "Task Complete",
                         message: "Clean Task Finished."
                     }
                 },
                 sass: {
                     options: {
-                        title: "Task Complete",
                         message: "SASS Task Finished."
                     }
                 },
                 webpack: {
                     options: {
-                        title: "Task Complete",
                         message: "Script Generated."
                     }
+                },
+                bake: {
+                    options: {
+                        message: "HTML Generated."
+                    }
+                },
+                imagemin: {
+                    options: {
+                        message: "Image Minified."
+                    }
+                },
+                copy: {
+                    options: {
+                        message: "File/Folders Copied."
+                    }
+                },
+                uglify: {
+                    options: {
+                        message: "Script Compressed."
+                    }
+                }
+            },
+
+            // Watch Task
+            watch: {
+                options: {
+                    spawn: false
+                },
+
+                // Test Environment
+                testHTML: {
+                    files: [configs.srcs.root + configs.srcs.test + '**/*.html'],
+                    tasks: ['bake:test', 'notify:bake']
+                },
+                testAssets: {
+                    files: [configs.srcs.root + configs.srcs.assets + '**/*.*'],
+                    tasks: ['copy:test', 'imagemin:test', 'notify:copy', 'notify:imagemin']
+                },
+                testSASS: {
+                    files: [configs.srcs.root + configs.srcs.scss + '**/*.scss'],
+                    tasks: ['sass:test', 'notify:sass']
+                },
+                testScript: {
+                    files: [configs.srcs.root + configs.srcs.scripts + '**/*.js'],
+                    tasks: ['webpack:test', 'notify:webpack']
+                },
+
+                // Development Environment
+                devHTML: {
+                    files: [configs.srcs.root + configs.srcs.html + '**/*.html'],
+                    tasks: ['bake:dev', 'notify:bake']
+                },
+                devAssets: {
+                    files: [configs.srcs.root + configs.srcs.assets + '**/*.*'],
+                    tasks: ['copy:dev', 'imagemin:dev', 'notify:copy', 'notify:imagemin']
+                },
+                devSASS: {
+                    files: [configs.srcs.root + configs.srcs.scss + '**/*.scss'],
+                    tasks: ['sass:dev', 'notify:sass']
+                },
+                devScript: {
+                    files: [configs.srcs.root + configs.srcs.scripts + '**/*.js'],
+                    tasks: ['webpack:dev', 'notify:webpack']
+                },
+
+                // Production Environment
+                prodHTML: {
+                    files: [configs.srcs.root + configs.srcs.html + '**/*.html'],
+                    tasks: ['bake:prod', 'notify:bake']
+                },
+                prodAssets: {
+                    files: [configs.srcs.root + configs.srcs.assets + '**/*.*'],
+                    tasks: ['copy:prod', 'imagemin:prod', 'notify:bake']
+                },
+                prodSASS: {
+                    files: [configs.srcs.root + configs.srcs.scss + '**/*.scss'],
+                    tasks: ['sass:prod', 'notify:sass']
+                },
+                prodScript: {
+                    files: [configs.srcs.root + configs.srcs.scripts + '**/*.js'],
+                    tasks: ['webpack:prod', 'notify:webpack']
                 }
             }
         };
@@ -173,8 +505,14 @@
         grunt.initConfig(gruntConfigs);
 
         /* ---------- | Register Tasks | ---------- */
+        // Test Environment Tasks
+        grunt.registerTask('test:watch', noserve ? ['concurrent:testWatch'] : ['browserSync:test', 'concurrent:testWatch']);
         grunt.registerTask('test', ['concurrent:test']);
+        // Development Environment Tasks
+        grunt.registerTask('dev:watch', noserve ? ['concurrent:devWatch'] : ['browserSync:dev', 'concurrent:devWatch']);
         grunt.registerTask('dev', ['concurrent:dev']);
+        // Production Environment Tasks
+        grunt.registerTask('default:watch', noserve ? ['concurrent:prodWatch'] : ['browserSync:prod', 'concurrent:prodWatch']);
         grunt.registerTask('default', ['concurrent:prod']);
     };
 })();
--- README.md
+++ README.md
@@ -1,19 +1,69 @@
 # Definite :: Elevate CSS Framework #
-Welcome to our beloved css framework, the "**Elevate**" css framework. Right now, this project is still being developed,
-so there is no documentation right now about using this css framework.
+Welcome to our beloved css framework, the "**Elevate**" css framework. Please read the instruction below to use and how to
+contributing to this framework.
 
-We will periodically announce when the framework is ready for test. Right now, this `README.md` file only guide you how to
-get to contribute with us.
+## Table of Contents ##
 
-## Contributing Guidelines ##
-First of all, we would like to notice you that the framework itself is built on top of Node.JS, so it will have a lot
-dependencies installs.
+* **Contributing**
+* **Setup**
+* **How To**
 
-And, we use the `BEM / Block-Element-Modifier` methodology for each style we created. Anyway, the file structures is currently
-use the recommendation of BEM file structure. You can refer it (https://en.bem.info/methodology/filestructure/#nested)[here].
+### Contributing ###
+Before begin contributing, please read this guidlines carefully. We will using **BEM** methodology for all code styling.
+Please refer to the BEM tutorial if you want to know more about BEM.
 
-The first step to start contributing is, just **Fork** this repo, and do a code then finally make a **PR / Pull Request** to
-this repo. Our team will review your changes and we will merge that if we accept your commits.
+### Setup ###
+Before using this framework, you have to install the requirements first. Please read this instructions for
+installing the dependencies.
 
-## More Information ##
-For more information about contributing, feel free for dropping us a message via email to developer@definite.co.id.
+#### 1. Install Node.JS ####
+You must have a Node.JS installed on the system. For installing Node.JS, you can go to the official site
+of Node.JS here, and select what OS type you prefer to install the Node.JS.
+
+#### 2. Clone This Repo ####
+After checking that Node.JS installed, you can clone this repo to your local machine. And then, open up
+terminal/command-prompt, change directory to where this cloned repo belonging, and type `npm install`.
+And, that's it! All fire up!
+
+### How To ###
+Before you begin coding, please note that we've created well-structured environment and directory hierarchy.
+We use 3 conditional environments, which is **Testing Environment**, **Development Environment** and **Production Environment**.
+
+We will describe what it's for below:
+
+- `Testing Environment`: This ENV purpose is to be used whenever you want to contribute to this project. We
+use this environment for developing this framework. So, don't use this environment for your development/staging/production
+project.
+- `Development Environment`: Use this ENV when you want to startup your project or developing some existing project.
+This is good when the team wants to start the project, fixing some bugs, etc.
+- `Production Environment`: For production, use this ENV to have the generated and minified files and folders.
+
+Beside the environment, take a note that we use **Grunt** as our task runner. So, if you un-familliar with Grunt,
+we prefer you can learn some basic in their official site.
+
+#### Grunt Command Lists ####
+After you install everything, as mentioned above, you can use our generated grunt command for start development.
+These commands are:
+
+- `grunt test`: This command is used whenever you want to start development/contribute for this frameworks.
+The flow of this command is, first the command will check and delete "**./framework-tests**" directory from root project,
+then generate a new files for tests. The public file is inside the **./framework-tests** directory. We recommend to not
+modifying the files in that folder, use **./srcs** directory instead for begin coding.
+- `grunt test:watch`: Run this command whenever you start developing the `grunt test`. This task will watch
+the change and run the `Browser Sync` module and starting the little web server automatically.
+- `grunt dev`: Same as `grunt test`, but the difference is the task doesn't create **./framework-tests** directory,
+instead **./dist** directory for public. So, your generated public test directory is **./dist**.
+- `grunt dev:watch`: Watching the file and folder changes in the **./srcs** directory.
+- `grunt`: This is the default task as we serve them for `Production Environment`. Use this task whenever you
+finished the project or want to release another version.
+- `grunt default:watch`: This watching task is used if you want to watch the current change on `Production Environment`
+environment. Please note that this will change any changed files or directories in **./srcs** directory.
+- `grunt clean`: For cleaning project root structure. **./dist** and **./framework-tests** directories will removed.
+
+*Note: For all watching task without **Browser Sync**, you can optionally pass `--noserve` parameters.*
+
+For now on, we only can help you how to use this framework, not the well-documented framework components, libraries
+or else. It's still on development. So, for any related questions about using the framework, feel free
+to ask us via emai to [mailto:developer@definite.co.id](Definite Developers Mailing List).
+
+Good luck and have fun!
